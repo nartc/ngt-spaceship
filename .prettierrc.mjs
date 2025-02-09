c26a35fd467cb208eeff0809394fbd782f7d8c7c
nartc <nartc7789@gmail.com>
2025-02-08 20:11:29 -0600

    format

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,5 +3,7 @@ export default {
   singleQuote: true,
   useTabs: true,
   htmlWhitespaceSensitivity: 'ignore',
+  tabWidth: 4,
+  printWidth: 120,
   plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
 };
